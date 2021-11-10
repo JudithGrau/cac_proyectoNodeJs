1d9eac299db996c77c7a59eb12b3122c2e84edd6
JudithGrau <judith.betina.grau@gmail.com>
2021-11-10 18:14:52 -0300

    Ejs, include, layout, modulo de productos

--- router.js
+++ router.js
@@ -1,16 +1,19 @@
 const express = require('express')
 const router = express.Router()
 
+const productos = require('./productos')
+
 router.get('/', (req,res)=>{
-    res.send('Index')
+    res.render('Index')
 })
 
 router.get('/productos', (req,res)=>{
-    res.send('Lista de productos!!')
+    /* console.log(productos.all()); */
+    res.render('productos/index', { productos: productos.all() })
 })
 
 router.get('/productos/:id', (req,res)=>{
-    res.send('Producto: ' + req.params.id)
+    res.render('productos/show',{ producto: productos.find(req.params.id) } )
 })
 
 module.exports = router
